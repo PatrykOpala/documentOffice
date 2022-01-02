b92c8d45bd66ea78fe8ba1b3f7973dbbb4f0d0d9
PatrykOpala <77116693+PatrykOpala@users.noreply.github.com>
2022-01-02 20:10:47 +0100

    0.0.2

--- src/app/dashboard/search-box/search-box.component.ts
+++ src/app/dashboard/search-box/search-box.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-box',
   templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.css']
+  styleUrls: ['./search-box.component.css'],
+  inputs: ['srcImageSearch', 'srcImageCancelSearch']
 })
 export class SearchBoxComponent implements OnInit {
 
   constructor() { }
+
+  srcImageSearch: string | null = null;
+  srcImageCancelSearch: string | null = null;
 
   ngOnInit(): void {
   }
